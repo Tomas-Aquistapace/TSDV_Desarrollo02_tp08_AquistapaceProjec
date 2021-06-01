b22157c60a798152985ace806ef02a88f8367abc
Tomas <Aquista35@gmail.com>
2021-06-01 18:55:29 -0300

    Main Menu agregado

---
+++ TP8 - Aquistapace Tomas/Assets/Scripts/Scene Managers/SceneButtonManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneButtonManager : MonoBehaviour
+{
+    public string sceneToPlay;
+
+    public void PlayScene()
+    {
+        SceneManager.LoadScene(sceneToPlay);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
